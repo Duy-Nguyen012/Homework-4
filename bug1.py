d7a0ff5d0defb4bbeceee5362efec7b4f0dd300c
Duy-Nguyen012 <144294062+Duy-Nguyen012@users.noreply.github.com>
2023-10-11 16:44:52 -0700

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -7,19 +7,20 @@ class Base:
 class Circle():
     def __init__(x, size):
         super().__init__(x, y, size)
+
     def draw(self):
-    return f"""
-        ({self.x}, {self.y})\n{self.size}
-, - ~ ~ ~ - ,
-, ' ' ,
-, ,
-, ,
-, ,
-, ,
-, ,
-, ,
-, ,
-, , '
-' - , _ _ _ , '
-"""
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
 

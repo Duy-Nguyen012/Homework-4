eb2e48873744119a7799d0f3523aafb18173d825
Duy-Nguyen012 <144294062+Duy-Nguyen012@users.noreply.github.com>
2023-10-12 09:49:00 -0700

    Update log.py

--- log.py
+++ log.py
@@ -3,7 +3,7 @@ import time
 def timestamp(func):
     def wrapper(*args, **kwargs):
         current_time = time.ctime()
-        print({current_time})
+        print(current_time)
         result = func(*args, **kwargs)
         print(f"Decorated Content: {result}")
         return result

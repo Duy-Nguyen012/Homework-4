6c22c976b0e9eff03c43489b4abf230bccc50148
Duy-Nguyen012 <144294062+Duy-Nguyen012@users.noreply.github.com>
2023-10-12 09:26:29 -0700

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,6 +1,10 @@
-def reverse_sort_dictionary(dic): # create a function called sort_directory which dic is the arugument
-    sorted_list = [] # create an empty list
-    for name,(phone_number, age) in sorted(dic.items(), key = lambda x: x[1][1]): #Iterate the rough the dictionary using sorted function, that sort based onn age of the dictionary
-        sorted_list.append((name,phone_number)) #append the name and phone number to the sorted_list
-        return sorted_list
+
+def reverse_sort_dictionary(input_dict):
+    # Sort the dictionary by keys (names) in reverse order
+    sorted_dict = sorted(input_dict.items(), key=lambda item: item[0], reverse=True)
+    
+    result_list = [(name, info[0]) for name, info in sorted_dict]
+    
+    return result_list
+
     
